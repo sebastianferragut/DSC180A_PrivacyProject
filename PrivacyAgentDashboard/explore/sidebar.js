a21f10f606a7880662be3a2fa9d44c946d43d3c9
averma2005 <a5verma@ucsd.edu>
2026-02-20 04:09:19 -0800

    Update sidebar.js

--- PrivacyAgentDashboard/explore/sidebar.js
+++ PrivacyAgentDashboard/explore/sidebar.js
@@ -6,7 +6,6 @@ function loadAdditional() {
 }
 
 document.getElementById('recommendationsBtn').addEventListener('click', function() {
-    const additional = document.getElementById("additional");
     const categoryIntro = document.getElementById("categoryIntro");
     const treemap = document.getElementById("treemap");
     
@@ -16,23 +15,16 @@ document.getElementById('recommendationsBtn').addEventListener('click', function
     }
     
     // Show category intro and treemap if they exist
-    if (categoryIntro) {
-        categoryIntro.classList.remove('hidden');
-    }
     if (treemap) {
         treemap.classList.remove('hidden');
     }
 });
 
 document.getElementById('crossPlatformBtn').addEventListener('click', function() {
-    const tutorial = document.getElementById("categoryIntro");
     const treemap = document.getElementById("treemap");
     const additional = document.getElementById("additional");
     
     // Hide category intro and treemap
-    if (tutorial) {
-        tutorial.classList.add('hidden');
-    }
     if (treemap) {
         treemap.classList.add('hidden');
     }
