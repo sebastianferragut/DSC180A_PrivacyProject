49365b0642cc8114f5b9490be4c99c71b923815b
averma2005 <a5verma@ucsd.edu>
2026-03-04 01:01:09 -0800

    Remove unnecessary JS code

--- PrivacyAgentDashboard/explore/sidebar.js
+++ PrivacyAgentDashboard/explore/sidebar.js
@@ -5,26 +5,16 @@ function loadAdditional() {
     return;
 }
 
+// const categoryIntro = document.getElementById("categoryIntro");
+const treemap = document.getElementById("treemapContainer");
+const additional = document.getElementById("additional");
+
 document.getElementById('recommendationsBtn').addEventListener('click', function() {
-    const categoryIntro = document.getElementById("categoryIntro");
-    const treemapSection = document.getElementById("treemap");
-    const treemap = document.getElementById("treemapContainer");
-    const additional = document.getElementById("additional");
-    
     if (additional) additional.classList.add('hidden');
-    if (categoryIntro) categoryIntro.classList.remove('hidden');
-    if (treemapSection) treemapSection.classList.remove('hidden');
     if (treemap) treemap.classList.remove('hidden');
 });
 
-document.getElementById('crossPlatformBtn').addEventListener('click', function() {
-    const categoryIntro = document.getElementById("categoryIntro");
-    const treemapSection = document.getElementById("treemap");
-    const treemap = document.getElementById("treemapContainer");
-    const additional = document.getElementById("additional");
-    
-    if (categoryIntro) categoryIntro.classList.add('hidden');
-    if (treemapSection) treemapSection.classList.add('hidden');
+document.getElementById('crossPlatformBtn').addEventListener('click', function() {  
     if (treemap) treemap.classList.add('hidden');
     if (additional) additional.classList.remove('hidden');
 });
